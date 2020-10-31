a1dff9d0c851574a800abb6b38e6549ba967a82b
JustinAndrade <j.andrade.john@gmail.com>
2020-10-31 03:21:33 -0400

    updated apiCall, changed setOff to pageHandler, also updated comments.

--- imports/ui/movielist/movielist.js
+++ imports/ui/movielist/movielist.js
@@ -1,208 +1,188 @@
-import './movielist.html';
-import { Template } from 'meteor/templating';
-import { Meteor } from 'meteor/meteor'
-import { Session } from 'meteor/session'
- 
+import "./movielist.html";
+import { Template } from "meteor/templating";
+import { Meteor } from "meteor/meteor";
+import { Session } from "meteor/session";
 
-if(Meteor.isClient){  
-  // Setting up some state for the application
-  Session.set('searchQuery', '')
-  Session.set('offset', 0)
-  Session.set('sortedBy', {
-    title: 'title',
+if (Meteor.isClient) {
+  // Setting up state for the application
+  // -> FROM HERE DOWN <- //
+  Session.set("searchQuery", "");
+  Session.set("offset", 0);
+  Session.set("sortedBy", {
+    title: "title",
     locations: false,
     release_year: false,
-  })
+  });
 
   // Page handlers
-  Session.set('next', true)
-  Session.set('prev', false)
-
+  Session.set("next", true);
+  Session.set("prev", false);
+  // -> FROM HERE UP <- //
 
   // Grabbing the getMovies function from the server side.
-  apiCall('title')
+  // This will sort by 'title' by default.
+  apiCall("title");
 
   Template.body.helpers({
     movies: () => {
-      return Session.get('movies')
+      return Session.get("movies");
     },
     nextPage: () => {
-      return Session.get('next')
+      return Session.get("next");
     },
     prevPage: () => {
-      return Session.get('prev')
-    }
+      return Session.get("prev");
+    },
+  });
 
-  })
-  
   Template.body.events({
-    // Searchbar
-    // Each key press will search for a result.
-    'keyup .searchbar': (event) => {
-      const query = event.target.value.toLowerCase()
-      const sort = getSort(Session.get('sortedBy'))
-      Session.set('searchQuery', query)
-      Session.set('offset', 0)
-      apiCall(sort)
-
+    // Searchbar which render results on key press.
+    "keyup .searchbar": (event) => {
+      // Setting query to lowercase so it will match the server request.
+      const query = event.target.value.toLowerCase();
+      const sort = getSort(Session.get("sortedBy"));
+      // Resetting state when searching.
+      Session.set("searchQuery", query);
+      Session.set("offset", 0);
+      Session.set("prev", false);
+      apiCall(sort);
     },
 
     // Grabs the next 25 movies
-    'click .next': () => {
-      Session.set('prev', true)
-      const sort = getSort(Session.get('sortedBy'))
-      let offset = Session.get('offset') + 25
-
-      Meteor.call('getMovies', sort, offset, Session.get('searchQuery'), Session.get('searchQuery'), (err, res) => {
-        if(err) {
-          console.log('error', err)
-        };
-    
-        // Generating google map links for each movie
-        res.data.forEach(movie => {
-            mapLinkGenerator(movie)
-            if(movie.locations === undefined){
-              movie.locations = ''
-            }
-          })
-
-        if(res.data.length > 0){
-          // Checking if next button should be rendered
-          if(res.data.length < 25){
-            Session.set('next', false)
-          }
-          // Initializing movies
-          Session.set('movies', res.data)
-          Session.set('offset', offset)
-        } else {
-          offset -= 25
-          Session.set('offset',offset)
-          Session.set('next', false)
-        }
-      })
+    "click .next": () => {
+      Session.set("prev", true);
+      pageHandler(Session.get("offset"), "next");
     },
-
 
     // Grabs the last 25 movies
-    'click .prev': () => {
-      Session.set('next', true)
-      const sort = getSort(Session.get('sortedBy'))
-      let offset = Session.get('offset')
-
-      if(offset > 0){
-        offset -= 25
-        Session.set('offset', offset)
-      }
-
-      // Checking if next button should be rendered
-      if(offset === 0){
-        Session.set('prev', false)
-      }
-
-      Meteor.call('getMovies', sort, offset, Session.get('searchQuery'), (err, res) => {
-        if(err) {
-          console.log('error', err)
-        };
-        // Generating google map links for each movie
-        res.data.forEach(movie => {
-            mapLinkGenerator(movie)
-            if(movie.locations === undefined){
-              movie.locations = ''
-            }
-          })
-        
-        // Initializing movies
-        Session.set('movies', res.data)
-      })
+    "click .prev": () => {
+      Session.set("next", true);
+      pageHandler(Session.get("offset"), "prev");
     },
-
 
     // Sort Year click handler
-    'click .sortYear': () => {
+    "click .sortYear": () => {
       const sortedBy = {
         title: false,
-        release_year: 'release_year',
+        release_year: "release_year",
         locations: false,
-      }
-      Session.set('offset', 0)
-      Session.set('sortedBy', sortedBy)
-      apiCall(sortedBy.release_year)
-    },    
-
+      };
+      Session.set("sortedBy", sortedBy);
+      pageHandler();
+      apiCall(sortedBy.release_year);
+    },
 
     // Sort Title click handler
-    'click .sortTitle': () => {
+    "click .sortTitle": () => {
       const sortedBy = {
-        title: 'title',
+        title: "title",
         release_year: false,
-        locations: false
-      }
-      Session.set('offset', 0)
-      Session.set('sortedBy', sortedBy)
-      apiCall(sortedBy.title)
+        locations: false,
+      };
+
+      Session.set("sortedBy", sortedBy);
+      pageHandler();
+      apiCall(sortedBy.title);
     },
-    
-    
+
     // Sort Locations click handler
-    'click .sortLocation': () => {
+    "click .sortLocation": () => {
       const sortedBy = {
         title: false,
         release_year: false,
-        locations: 'locations'
-      }
+        locations: "locations",
+      };
 
-      // const offset = Session.get('offset')
-      Session.set('offset', 0)
-      Session.set('sortedBy', sortedBy)
-      apiCall(sortedBy.locations)
-    },    
-  })
+      Session.set("sortedBy", sortedBy);
+      pageHandler();
+      apiCall(sortedBy.locations);
+    },
+  });
 }
-
-
 
 //////////////////
 // MY FUNCTIONS //
 //////////////////
 
-
-// Function to generate a google page with location of movie
+// Function to generate a google maps link corresponding to where the film was filmed.
 function mapLinkGenerator(movie) {
-  return movie.href = `https://www.google.com/maps/search/?api=1&query=${movie.locations}+San+Francisco+CA`
+  return (movie.href = `https://www.google.com/maps/search/?api=1&query=${movie.locations}+San+Francisco+CA`);
 }
 
 // Function that sorts through the sortedBy OBJ and returns the current truthy value
-function getSort(obj){
-  for(let i in obj){
-    if(obj[i] !== false){
-      return obj[i]
+function getSort(obj) {
+  for (let i in obj) {
+    if (obj[i] !== false) {
+      return obj[i];
     }
   }
 }
 
-// Dynamic query function that allows you to pull specific information from the database, uses state set from Session.
-function apiCall(sortedBy){
-  Meteor.call('getMovies', sortedBy, Session.get('offset'), Session.get('searchQuery'), (err, res) => {
-    if(err) {
-      console.log('error', err)
-    };
-    if(res.data.length > 1){
-      if(res.data.length < 25){
-        Session.set('next', false)
-      } else if(res.data.length === 25) {
-        Session.set('next', true)
-      }
-
-      // Generating google map links for each movie
-      res.data.forEach(movie => {
-        mapLinkGenerator(movie)
-        if(movie.locations === undefined){
-          movie.locations = ''
-        }
-      })
+// Handles the offset to move through pages.
+function pageHandler(offset, command) {
+  let newOffset = 0;
+  const sort = getSort(Session.get("sortedBy"));
+  // Handling if user clicks 'prev page'
+  if (command === "prev") {
+    if (offset > 0) {
+      offset -= 25;
+      Session.set("offset", offset);
     }
-    
-    // Initializing movies
-    Session.set('movies', res.data)
-  })
+
+    // Checking if next button should be rendered
+    if (offset === 0) {
+      Session.set("prev", false);
+    }
+    apiCall(sort);
+    return;
+
+    // Handling if user clicks 'next page'
+  } else if (command === "next") {
+    offset += 25;
+    Session.set("offset", offset);
+    apiCall(sort);
+  } else {
+    Session.set("offset", newOffset);
+    return newOffset;
+  }
+}
+
+// Dynamic query function that allows you to pull specific information from the database, uses state set from Session.
+// sortedBy will be 'title' by default
+function apiCall(sortedBy) {
+  const offset = Session.get("offset");
+  Meteor.call(
+    "getMovies",
+    sortedBy,
+    Session.get("offset"),
+    Session.get("searchQuery"),
+    (err, res) => {
+      if (err) {
+        console.log("error", err);
+        // If we are receiving a list of movies run this.
+      } else if (res.data.length > 1) {
+        if (res.data.length < 25) {
+          Session.set("next", false);
+        } else if (res.data.length === 25) {
+          Session.set("next", true);
+        }
+        // Generating google map links for each movie.
+        res.data.forEach((movie) => {
+          mapLinkGenerator(movie);
+          if (movie.locations === undefined) {
+            movie.locations = "";
+          }
+        });
+
+        // Set the movies to state to render.
+        Session.set("movies", res.data);
+        Session.set("offset", offset);
+        // If our list of movies is empty run this.
+      } else {
+        offset -= 25;
+        Session.set("offset", offset);
+        Session.set("next", false);
+      }
+    }
+  );
 }
--- server/main.js
+++ server/main.js
@@ -1,31 +1,33 @@
-import { HTTP } from 'meteor/http'
-import { Meteor } from 'meteor/meteor' 
-
+import { HTTP } from "meteor/http";
+import { Meteor } from "meteor/meteor";
 
 // Fetching Books from third party API
-if(Meteor.isServer){
+if (Meteor.isServer) {
   // Will run on inital load
-  Meteor.startup(function() {
+  Meteor.startup(function () {
     Meteor.methods({
       // method to request movies based on sorting, page, and possible query
-      getMovies: function(sortedBy, offset, query) {
-        let result = ''
-        if(query){
-          if(query && sortedBy){
-            result = HTTP.get(`https://data.sfgov.org/resource/wwmu-gmzc.json?$query=SELECT * WHERE starts_with(lower(title), '${query}') ORDER BY ${sortedBy} LIMIT 25 OFFSET ${offset}`)
-            if(result.data.length === 0){
-              result = HTTP.get(`https://data.sfgov.org/resource/wwmu-gmzc.json?$where=release_year = ${Number(query)} LIMIT 25`)
-            }
-            return result
+      getMovies: function (sortedBy, offset, query) {
+        let result = "";
+        if (query) {
+          result = HTTP.get(
+            `https://data.sfgov.org/resource/wwmu-gmzc.json?$query=SELECT * WHERE starts_with(lower(title), '${query}') ORDER BY ${sortedBy} LIMIT 25 OFFSET ${offset}`
+          );
+          if (result.data.length === 0) {
+            result = HTTP.get(
+              `https://data.sfgov.org/resource/wwmu-gmzc.json?$where=release_year = ${Number(
+                query
+              )} LIMIT 25`
+            );
           }
-        }
-        if(sortedBy) {
-          result = HTTP.get(`https://data.sfgov.org/resource/wwmu-gmzc.json?$order=${sortedBy} asc LIMIT 25 OFFSET ${offset.toString()}`)
+          return result;
         } else {
-          result = HTTP.get(`https://data.sfgov.org/resource/wwmu-gmzc.json?$order=title asc LIMIT 25 OFFSET ${offset.toString()}`)
+          result = HTTP.get(
+            `https://data.sfgov.org/resource/wwmu-gmzc.json?$order=${sortedBy} asc LIMIT 25 OFFSET ${offset.toString()}`
+          );
+          return result;
         }
-        return result
       },
-    })
-  })
+    });
+  });
 }
